0960fb709ef59136e6d53b125ea7509e262100ff
IgnazioFausto <nachojam777@gmail.com>
2021-12-10 23:45:09 -0300

    archivos fs

--- Clase-2-Manejo-de-archivos.js
+++ Clase-2-Manejo-de-archivos.js
@@ -1,4 +1,4 @@
-const {promises: fs} = require('fs');
+const { promises: fs } = require('fs');
 
 class Contenedor {
     constructor(ruta) {
@@ -16,21 +16,21 @@ class Contenedor {
             newId = objs[objs.length - 1].id + 1
         }
 
-        const newObj = {...obj, id: newId}
+        const newObj = { ...obj, id: newId }
         objs.push(newObj);
 
         try {
-            
-            await fs.writeFile(this.ruta, JSON.stringify(objs, null, 2));
+
+            await fs.writeFile(this.ruta, JSON.stringify(objs, null, 3));
             return newId;
 
         } catch (error) {
-            throw new Error (`Error al guardar: ${error}`);
+            throw new Error(`Error al guardar: ${error}`);
         }
     }
-    
+
     async getById(id) {
-         
+
         const objs = await this.getAll();
         const obj = objs.find(x => x.id == id);
         return obj;
@@ -38,20 +38,54 @@ class Contenedor {
 
     async getAll() {
         try {
-            
+
             const objs = await fs.readFile(this.ruta, 'utf-8');
             return JSON.parse(objs);
-             
+
         } catch (error) {
             return []
         }
     }
-    deleteById() {
+    async deleteById(id) {
+        /*  const objs = await this.getAll();
+         const obj = objs.map(x => x.id != id);
+       
+        console.log(obj); */
+
+        await fs.readFile('./productos.txt', function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            var prod = data.toString();
+            prod = JSON.parse(prod);
+            // Leer los datos para eliminar
+            for (var i = 0; i < prod.data.length; i++) {
+                if (id == prod.data[i].id) {
+                    prod.data.splice(i, 1);
+                }
+            }
+            prod.total = prod.data.length;
+            var str = JSON.stringify(prod);
+            // Luego escribe los datos en
+            fs.writeFile('./productos.txt', str, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+                console.log("---------- eliminado correctamente ------------");
+            })
+        })
 
     }
-    deleteAll() {
+    async deleteAll() {
+        const deletedAll = fs.writeFile('./productos.txt', '', err => {
+            if (err) console.log(err);
+            else {
+                console.log("\nDatos borrados,", deletedAll);
+            }
+        });
 
-    } 
+
+    }
 }
 
 module.exports = Contenedor
--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const p1 = {
     title: "Escuadra",
     price: 123.45,
     thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png",
-  }
-  
+}
+
 const p2 = {
     title: "Calculadora",
     price: 234.56,
     thumbnail: "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
 }
-  
+
 const p3 = {
     title: "Globo Terráqueo",
     price: 345.67,
@@ -26,13 +26,17 @@ async function main() {
     let objs = await contenedor.getAll();
     console.log(objs);
 
-    console.log('Guado Producto 1');
+    console.log('Guardo Producto 1');
     let idp1 = await contenedor.save(p1)
     console.log('id de p1: ', idp1);
 
-    console.log('Guado Producto 2');
+    console.log('Guardo Producto 2');
     let idp2 = await contenedor.save(p2)
     console.log('id de p2: ', idp2);
+    
+    console.log('Guardo Producto 3');
+    let idp3 = await contenedor.save(p3)
+    console.log('id de p3: ', idp3);
 
     console.log('Muestro Todo');
     objs = await contenedor.getAll();
@@ -41,7 +45,39 @@ async function main() {
     console.log('Busco por ID');
     const res = await contenedor.getById(2);
     console.log('Resultado: ', res);
+    /* 
+        console.log('Elimino por ID');
+        const prodDeleted = await contenedor.deleteById(2)
+        console.log(`Eliminado producto del Id: ${prodDeleted}`); */
 
+    console.log('Elimino todo');
+    await contenedor.deleteAll();
+
+    console.log('Muestro el archivo vacío');
+    objs = await contenedor.getAll();
+    console.log(objs);
+
+    console.log('Guardo de nuevo el Producto 1');
+    let idP1 = await contenedor.save(p1)
+    console.log('id de p1: ', idP1);
+
+    console.log('Guardo de nuevo el Producto 2');
+    let idP2 = await contenedor.save(p2)
+    console.log('id de p2: ', idP2);
+
+    console.log('Muestro con los dos Productos agregados');
+    objs = await contenedor.getAll();
+    console.log(objs);
+
+    console.log('Elimino por Id el producto 2')
+    await contenedor.deleteById(2);
+    console.log('Producto borrado');
+
+    console.log('Muestro con el producto elegido, borrado');
+    objs = await contenedor.getAll();
+    console.log(objs);
+    
+    
 }
 
 main()
